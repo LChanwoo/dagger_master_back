90f3561e7d838a2a383bc6013ee4295f64ee7af2
CHANWOO LEE <chw210601@gmail.com>
2024-01-20 14:43:26 +0900

    mail 콘트롤러 swagger 설명 추가

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -3,6 +3,7 @@ import { MailService } from './mail.service';
 import { User } from 'src/common/decorators/user.decorator';
 import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 import { PostMailDto } from './dto/postMail.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('mail')
 @UseGuards(AuthenticatedGuard)
@@ -10,6 +11,10 @@ export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Post()
+  @ApiOperation({ summary: '메일 보내기' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 400, description: 'body 확인 필요' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
   async postMail(@User() user: any, @Body() body: PostMailDto) {
     const { user_id, title, contents, item_id, qty } = body;
     return await this.mailService.postMail(
@@ -22,19 +27,31 @@ export class MailController {
   }
 
   @Get()
+  @ApiOperation({ summary: '메일 리스트 조회' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
   async getMailList(@User() user: any) {
     return await this.mailService.getMailList(user.USER_ID);
   }
 
   @Get('/:mail_id')
+  @ApiOperation({ summary: '메일 읽기' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
   async checkAndReadMail(@User() user: any, @Param('mail_id') mail_id: number) {
     return await this.mailService.checkAndReadMail(user.USER_ID, mail_id);
   }
   @Get('/:mail_id/item')
+  @ApiOperation({ summary: '메일 아이템 읽기' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
   async getMailItem(@User() user: any, @Param('mail_id') mail_id: number) {
     return await this.mailService.getMailItem(user.USER_ID, mail_id);
   }
   @Get('/all/mails')
+  @ApiOperation({ summary: '모든 메일 읽기 & 아이템 수령 & 모든 메일 삭제' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
   async getAllMail(@User() user: any) {
     const { USER_ID } = user;
     console.log(USER_ID);
